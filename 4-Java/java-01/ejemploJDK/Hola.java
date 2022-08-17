5040e0d300315142dae5cee8aa534fe3f88ae919
Abel <abelgenne@gmail.com>
2022-08-17 09:43:09 -0500

    Introduccion a Java con JDK e Intelliji

---
+++ 4-Java/java-01/ejemploJDK/Hola.java
@@ -0,0 +1,17 @@
+public class Hola {
+//esto es una clase  del JDK Java
+//String miVariable = "Hola mundo java";
+
+
+
+              public static void main(String arg []){
+              //String miVariable = "Hola mundo java saludo a: ";
+              String miNombre = "Abel Lopez";    
+              //es la declaracion del metodo main 
+               System.out.println ("Hola saludos a: " + miNombre);
+
+}        
+
+}
+
+
