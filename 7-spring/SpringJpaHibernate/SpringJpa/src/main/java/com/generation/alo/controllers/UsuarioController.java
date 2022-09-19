a5e6c4118f11c8fade39ee594a33aa6d47b936ca
Abel <abelgenne@gmail.com>
2022-09-19 09:30:05 -0500

    Creacion de una API-Rest

--- 7-spring/SpringJpaHibernate/SpringJpa/src/main/java/com/generation/alo/controllers/UsuarioController.java
+++ 7-spring/SpringJpaHibernate/SpringJpa/src/main/java/com/generation/alo/controllers/UsuarioController.java
@@ -1,12 +1,18 @@
 package com.generation.alo.controllers;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.alo.models.UsuarioModel;
@@ -14,6 +20,8 @@ import com.generation.alo.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.PUT })
 
 
 
@@ -32,6 +40,29 @@ public class UsuarioController {
 	public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
         return this.usuarioService.guardarUsuario(usuario);
     }
+	@GetMapping( path = "/{id}")
+	public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerPorId(id);
+    }
+	@GetMapping("/query")
+	public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+	@DeleteMapping( path = "/{id}")
+	public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok){
+            return "Se eliminó el usuario con id " + id;
+        }else{
+            return "No pudo eliminar el usuario con id" + id;
+        }
+    }
+
+
+
+
+
+
 
 
 
--- 7-spring/SpringJpaHibernate/SpringJpa/src/main/java/com/generation/alo/services/UsuarioService.java
+++ 7-spring/SpringJpaHibernate/SpringJpa/src/main/java/com/generation/alo/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.generation.alo.services;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,27 @@ public class UsuarioService {
 	public UsuarioModel guardarUsuario(UsuarioModel usuario){
         return usuariorepository.save(usuario);
     }
+	
+	
+	public Optional<UsuarioModel> obtenerPorId(Long id){
+	        return usuariorepository.findById(id);
+	    }
+	
+	public ArrayList<UsuarioModel>  obtenerPorPrioridad(Integer prioridad) {
+	        return usuariorepository.findByPrioridad(prioridad);
+	    }
+	public boolean eliminarUsuario(Long id) {
+        try{
+            usuariorepository.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
+
+
+	
+	
 
 	
 
